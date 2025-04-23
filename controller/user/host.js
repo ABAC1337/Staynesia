15c4ab90296506df82748dd9409b0e4562eecdef
ABAC <yunanwu123@gmail.com>
2025-04-23 22:56:46 +0700

    update host controller

--- controller/user/host.js
+++ controller/user/host.js
@@ -1,3 +1,20 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+const createHost = async (name, email, hashPass) => {
+    return await prisma.Guest.create({
+        name : name,
+        email : email,
+        password : hashPass
+    })
+}
+const findHostEmail = async (payload) => {
+    const guest = await prisma.Guest.findOne({
+        email : payload
+    })
+}
+
+module.exports = {
+    findHostEmail,
+    createHost
+}
