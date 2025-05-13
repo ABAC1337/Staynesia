5dd41fbd25481d919b5a408849e76351d6ea8b27
ABAC <yunanwu123@gmail.com>
2025-05-13 11:20:20 +0700

    add listing to root

--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,19 @@
-const apiUser = require('./api/user')
 const express = require('express')
 const rootRoutes = express.Router()
 
-rootRoutes.use('/', apiUser)
+const listingRoute = require('./api/listing')
+const guestRoute = require('./api/guest')
+const registerRoute = require('./api/register')
+const loginRoute = require('./api/login')
 
+rootRoutes.get('/', (req, res) => {
+    res.send('Staynesia Api')
+})
+
+rootRoutes.use('/', registerRoute)
+rootRoutes.use('/', loginRoute)
+rootRoutes.use('/', guestRoute)
+rootRoutes.use('/', listingRoute)
 
 module.exports = {
     rootRoutes
