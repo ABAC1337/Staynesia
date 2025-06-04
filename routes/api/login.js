b314d95df589090e7565a16bf97a3a28b8254aad
ABAC <yunanwu123@gmail.com>
2025-06-05 06:41:15 +0700

    rearrange directory

--- routes/api/login.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const login = require('../../controller/loginController')
-
-router.post('/login-guest', login.loginGuest)
-router.post('/login-host', login.loginHost)
-
-module.exports = router;
