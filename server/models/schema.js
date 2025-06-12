8f37c5b92472eb9b70ed965ff8df09261d552604
ABAC <yunanwu123@gmail.com>
2025-06-12 17:24:39 +0700

    update model schema

--- server/models/schema.js
+++ server/models/schema.js
@@ -5,49 +5,54 @@ const { Schema } = mongoose;
 const GuestSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: [true, "Name is required field"]
     },
     username: {
         type: String,
-        required: true
+        required: [true, "Username is required field"],
+        unique: true
     },
     email: {
         type: String,
-        required: true,
+        required: [true, "Email is required field"],
         unique: true,
     },
     hashPassword: {
         type: String,
-        required: true
+        required: [true, "Password is required field"]
     },
     imageUrl: {
         type: String
     },
     bookings: [{ type: Schema.Types.ObjectId, ref: 'Booking' }],
     reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
-    wishlists: [{ type: Schema.Types.ObjectId, ref: 'Wishlist' }]
+    wishlists: [{ type: Schema.Types.ObjectId, ref: 'Wishlist' }],
+    payments: [{ type: Schema.Types.ObjectId, ref: 'Payment' }]
 }, { timestamps: true });
 
 // Host Schema
 const HostSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: [true, "Name is required field"]
     },
     username: {
         type: String,
-        required: true
+        required: [true, "Username is required field"],
+        unique: true
     },
     email: {
         type: String,
-        required: true,
+        required: [true, "Email is required field"],
         unique: true,
     },
     hashPassword: {
         type: String,
-        required: true
+        required: [true, "Password is required field"]
     },
-    imageUrl: String,
+    imageUrl: {
+        type: String
+    },
     listings: [{ type: Schema.Types.ObjectId, ref: 'Listing' }]
 }, { timestamps: true });
 
@@ -56,70 +61,64 @@ const ListingSchema = new Schema({
     location: {
         province: {
             type: String,
-            required: true
+            required: [true, "Province is required field!"]
         },
         city: {
             type: String,
-            required: true
+            required: [true, "City is required field!"]
         },
         address: {
             type: String,
-            required: true
+            required: [true, "Address is required field!"]
         }
     },
     category: {
         type: String,
-        required: true
+        required: [true, "Category is required field!"]
     },
     title: {
         type: String,
-        required: true
+        required: [true, "Title is required field!"]
     },
     description: {
         type: String,
-        required: true
+        required: [true, "Description is required field!"]
     },
     rules: {
         checkIn: {
             type: String,
-            required: true
+            required: [true, "Rules checkIn is required field!"]
         },
         checkOut: {
             type: String,
-            required: true
+            required: [true, "Rules checkOut is required field!"]
         },
         nightTime: {
-            start: {
-                type: String, 
-                required: true
-            },
-            end: {
-                type: String,
-                required: true
-            }
+            type: [String],
+            required: [true, "Rules quite hour is required field!"]
         },
         additional: {
             type: String,
-            required: true
+            required: [true, "Additional rules is required field!"]
         }
     },
     imgUrl: {
         type: [String],
-        required: true
+        required: [true, "Images is required field!"]
     },
     facility: {
         type: [String],
-        required: true
+        required: [true, "Facility is required field!"]
     },
     capacity: {
         type: Number,
-        required: true
+        required: [true, "Capacity is required field!"]
     },
     price: {
         type: Number,
-        required: true
+        required: [true, "Price is required field!"]
     },
-    host : {type: Schema.Types.ObjectId, ref: 'Host'},
+    host: { type: Schema.Types.ObjectId, ref: 'Host' },
     bookings: [{ type: Schema.Types.ObjectId, ref: 'Booking' }],
     reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
     wishlists: [{ type: Schema.Types.ObjectId, ref: 'Wishlist' }]
@@ -129,21 +128,28 @@ const ListingSchema = new Schema({
 const BookingSchema = new Schema({
     checkIn: {
         type: Date,
-        required: true
+        required: [true, "Check in is required field!"]
     },
     checkOut: {
         type: Date,
-        required: true
+        required: [true, "Check out is required field!"]
     },
     numGuest: {
         type: Number,
-        required: true
+        required: [true, "Number of guest is required field!"]
     },
     totalPrice: {
         type: Number,
-        required: true
+        required: [true, "Total price is required field!"]
     },
-    statusBooking: String,
+    statusBooking: {
+        type: String,
+        default: "Pending",
+        enum: {
+            values: ["Pending", "Confirmed", "Succeeded"],
+            message: "This status does not exist"
+        }
+    },
     guestId: { type: Schema.Types.ObjectId, ref: 'Guest' },
     listingId: { type: Schema.Types.ObjectId, ref: 'Listing' },
     payment: { type: Schema.Types.ObjectId, ref: 'Payment' }
@@ -153,24 +159,36 @@ const BookingSchema = new Schema({
 const PaymentSchema = new Schema({
     paymentMethod: {
         type: Number,
-        required: true
+        required: [true, "Payment method is required field!"]
     },
-    paymentStatus: String,
+    paymentStatus: {
+        type: String,
+        default: "Pending",
+        enum: {
+            values: ["Pending", "Success", "Canceled"],
+            message: "This status does not exist"
+        }
+    },
     amount: {
         type: Number,
-        required: true
+        required: [true, "Amount is required field!"]
     },
     paidAt: {
         type: Number
     },
     bookingId: { type: Schema.Types.ObjectId, ref: 'Booking' },
+    guestId: { type: Schema.Types.ObjectId, ref: 'Guest' }
 }, { timestamps: true });
 
 // Review Schema
 const ReviewSchema = new Schema({
     guestId: { type: Schema.Types.ObjectId, ref: 'Guest' },
     listingId: { type: Schema.Types.ObjectId, ref: 'Listing' },
-    rating: Number,
+    rating: {
+        type: Number,
+        min: [1, "Rating must be 1.0 or above"],
+        max: [5, "Rating must be 5.0 or below"]
+    },
     reviewText: String
 }, { timestamps: true });
 
