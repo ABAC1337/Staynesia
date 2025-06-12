5f02f957e4146289e11ffa840d34b4b5fb5f1da2
ABAC <yunanwu123@gmail.com>
2025-06-12 17:21:18 +0700

    rearrange folder

--- server/api/v1/routes/api/register.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const register = require('../../controller/registerController')
-
-router.post('/register-guest', register.guestRegister)
-router.post('/register-host', register.hostRegister)
-
-module.exports = router;
