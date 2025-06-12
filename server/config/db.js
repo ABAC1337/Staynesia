3de9a21a7c2bae5e0f7fb214a31229b793ba87d8
ABAC <yunanwu123@gmail.com>
2025-06-12 17:24:48 +0700

    update config

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ mongoose.set('strictQuery', false);
 
 const connect = async () =>{
     try {
-        const conn = await mongoose.connect(process.env.DATABASE_URL2);
+        const conn = await mongoose.connect(process.env.DATABASE_URL);
         console.log(`DB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
