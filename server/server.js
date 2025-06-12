267f203d628708e5a6b0986b5823efc59e6d3ffc
ABAC <yunanwu123@gmail.com>
2025-06-12 17:26:21 +0700

    update server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv')
-const router = require('./routes/root')
+const router = require('./api/v1/routes/routes')
+const errorHandler = require('./api/v1/middleware/errorHandler')
 const db = require('./config/db')
 const app = express();
-dotenv.config()
+dotenv.config({path:'./config.env'})
 const PORT = 4000
 
 app.use(express.json());
 
-app.use('/api',router.rootRoutes)
-
+app.use('/api/v1',router)
+app.use(errorHandler)
 
 db.connect().then(() => {
     app.listen(PORT);
