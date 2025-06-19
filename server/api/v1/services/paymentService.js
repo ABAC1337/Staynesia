03bd157dae9a63b763325269857ebb69a0ea6ffa
ABAC <yunanwu123@gmail.com>
2025-06-20 00:00:25 +0700

    update service (payment)

--- server/api/v1/services/paymentService.js
+++ server/api/v1/services/paymentService.js
@@ -1,7 +1,24 @@
 const paymentRepo = require('../repositories/paymentRepository')
 const ErrorHandler = require('../../../utils/errorHandler') 
+const errorHandler = require('../middleware/errorHandler')
 
 const createPayment = async (data) => {
     if (!data) throw new ErrorHandler('Value not found', 404)
-    
+    return await paymentRepo.createPayment(data)
+}
+
+const updatePayment = async (id, data) => {
+    if (!id || !data) throw new errorHandler('Payment not found', 404)
+    return await paymentRepo.updatePayment(id, data)
+}
+
+const deletePayment = async (id) => {   
+    if (!id) throw new errorHandler('Payment not found', 404)
+    return await paymentRepo.deletePayment(id)
+}
+
+module.exports = {
+    createPayment,
+    updatePayment,
+    deletePayment
 }
