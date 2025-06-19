ecaa4ff789afd18a8dbe1cefdd7c2a34f94c7c35
ABAC <yunanwu123@gmail.com>
2025-06-20 00:00:12 +0700

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv')
 const router = require('./api/v1/routes/routes')
 const errorHandler = require('./api/v1/middleware/errorHandler')
 const db = require('./config/db')
 const app = express();
-dotenv.config({path:'./config.env'})
-const PORT = 4000
+dotenv.config()
+const PORT = 3000
+app.use(cors())
 
 app.use(express.json());
 
