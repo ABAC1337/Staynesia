3b157e94a80df3fde7c5ab3131e9fb8dfe280f85
ABAC <yunanwu123@gmail.com>
2025-06-25 21:53:51 +0700

    update routes.js

--- server/api/v1/routes/routes.js
+++ server/api/v1/routes/routes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const authMiddleware = require('../middleware/authHandler')
 const auth = require('./auth/auth')
-
-// const private = require('./private')
-// const public = require('./public')
+const private = require('./private/index')
+const public = require('./public/index')
 
 router.use('/auth', auth)
+router.use('/private', authMiddleware.authToken, private)
+router.use('/public', public)
 
 
 module.exports = router;
