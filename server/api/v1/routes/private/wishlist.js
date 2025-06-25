d65e0f30616d34babbdf5f3b1d0b3d23845321a5
ABAC <yunanwu123@gmail.com>
2025-06-25 21:49:20 +0700

    deleting files due to rearrange files

--- server/api/v1/routes/private/wishlist.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router()
-const wishlistController = require('../../controller/wishlistController')
-
-router.post('/', wishlistController.createWishlist)
-router.delete('/:id', wishlistController.deleteWishlist)
-
-module.exports = router
-
