6f196886c68a4a7ccf65d1d3fb4033f949fbe96e
ABAC <yunanwu123@gmail.com>
2025-06-28 21:07:10 +0700

    update controllers (booking, payment, review, wishlist)

--- server/api/v1/controller/bookingController.js
+++ server/api/v1/controller/bookingController.js
@@ -2,21 +2,21 @@ const asyncHandler = require('../../../utils/asyncHandler')
 const bookingService = require('../services/bookingService')
 
 const createBooking = asyncHandler(async (req, res, next) => {
-    const booking = await bookingService.createBooking(req.body)
+    await bookingService.createBooking(req.body)
     return res.status(201).json({
         message: 'Booking Created'
     })
 })
 
 const updateBooking = asyncHandler(async (req, res, next) => {
-    const booking = await bookingService.updateBooking(req.paras.id, req.body)
+    await bookingService.updateBooking(req.paras.id, req.body)
     return res.status(200).json({
         message: "Booking Updated"
     })
 })
 
 const deleteBooking = asyncHandler(async (req, res, next) => {
-    const booking = await bookingService.deleteBooking(req.params.id)
+    await bookingService.deleteBooking(req.params.id)
     return res.status(200).json({
         message: "Booking Deleted"
     })
--- server/api/v1/controller/reviewController.js
+++ server/api/v1/controller/reviewController.js
@@ -2,21 +2,21 @@ const asyncHandler = require('../../../utils/asyncHandler')
 const reviewService = require('../services/reviewService')
 
 const createReview = asyncHandler(async (req, res, next) => {
-    const review = await reviewService.createReview(req.body)
+    await reviewService.createReview(req.body)
     return res.status(201).json({
         message: 'Review created',
     })
 })
 
 const updateReview = asyncHandler(async (req, res, next) => {
-    const review = await reviewService.updateReview(req.params.id, req.body)
+    await reviewService.updateReview(req.params.id, req.body)
     return res.status(200).json({
         message: 'Review updated',
     })
 })
 
 const deleteReview = asyncHandler(async (req, res, next) => {
-    const review = await reviewService.deleteReview(id)
+    await reviewService.deleteReview(id)
     return res.status(200).json({
         message: 'Review deleted'
     })
