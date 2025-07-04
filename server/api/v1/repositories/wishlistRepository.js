e87f670519f5a6fd2ca536885ea6f2217f88a648
ABAC <yunanwu123@gmail.com>
2025-07-05 01:29:55 +0700

    update wishlist repository (add findone)

--- server/api/v1/repositories/wishlistRepository.js
+++ server/api/v1/repositories/wishlistRepository.js
@@ -4,11 +4,16 @@ const createWishlist = async (data) => {
     return await DB.Wishlist.create(data)
 }
 
+const findOneWishlist = async (data) => {
+    return await DB.Wishlist.findOne(data)
+}
+
 const deleteWishlist = async (id) => {
     return await DB.Wishlist.findByIdAndDelete(id)
 }
 
 module.exports = {
     createWishlist,
+    findOneWishlist,
     deleteWishlist
 }
