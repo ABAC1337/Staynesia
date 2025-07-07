759a21a0f821ae9fe0cc73ce9e7352ea0b3a0832
ABAC <yunanwu123@gmail.com>
2025-07-08 03:02:42 +0700

    update route (payment)

--- server/api/v1/routes/private/routes/payment.js
+++ server/api/v1/routes/private/routes/payment.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const paymentController = require('../../../controller/paymentController');
 
 router.post('/create', paymentController.createPayment)
-router.patch('/notification', paymentController.updatePayment)
+router.post('/notification', paymentController.updatePayment)
 router.route('/:id')
     .delete(paymentController.deletePayment)
 
