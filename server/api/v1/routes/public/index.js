2c79189fafd25ea0b7d25caeadbf8b144f9d2cb4
ABAC <yunanwu123@gmail.com>
2025-07-09 01:01:50 +0700

    update route index

--- server/api/v1/routes/public/index.js
+++ server/api/v1/routes/public/index.js
@@ -2,14 +2,8 @@ const express = require("express");
 const router = express.Router();
 const listing = require("./listing");
 const midtrans = require("./midtrans")
-const multer = require('../../middleware/multer')
 
 router.use("/listing", listing);
 router.use("/midtrans", midtrans)
-router.use("/upload", multer.upload.array('images', 5), (req, res) => {
-  if (!req.files || req.files.length === 0)
-    return res.status(404).json('Failed')
-  return res.status(200).json('Success')
-});
 
 module.exports = router;
