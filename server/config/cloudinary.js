8aad70d09c92a2b1023ad0f0a9055cae799bc327
ABAC <yunanwu123@gmail.com>
2025-07-09 01:01:41 +0700

    add new config cloudinary

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,21 +1,10 @@
-const cloudinary = require('cloudinary').v2
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require("cloudinary").v2;
+require('dotenv').config();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure: true
-})
+});
 
-const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: 'staynesia',
-        allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
-        use_filename: true,
-        unique_filename: false,
-    }
-})
-
-module.exports = { cloudinary, storage }
+module.exports = cloudinary;
