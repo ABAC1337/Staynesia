f9cf020e78f1baa9a79bfcc3ef500a2e4d09e929
ABAC <yunanwu123@gmail.com>
2025-07-08 21:54:11 +0700

    update multer middleware

--- server/api/v1/middleware/multer.js
+++ server/api/v1/middleware/multer.js
@@ -1,20 +1,19 @@
-const { log } = require("console");
+// const { log } = require("console");
+// const path = require("path");
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, path.resolve(__dirname,"../../../ListingUploads"));
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(
+//       null,
+//       file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+//     );
+//   },
+// });
 const multer = require("multer");
-const path = require("path");
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.resolve(__dirname,"../../../ListingUploads"));
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
-    );
-  },
-});
-
-const upload = multer({ storage: storage });
+const upload = multer({ storage: cloudinaryConfig.storage });
 
 module.exports = upload;
