beed22879a64536dfb42ee868ff7258964362f46
ABAC <yunanwu123@gmail.com>
2025-07-08 07:50:13 +0700

    update route routes (add checking api works)

--- server/api/v1/routes/routes.js
+++ server/api/v1/routes/routes.js
@@ -5,6 +5,9 @@ const auth = require('./auth/auth')
 const private = require('./private/index')
 const public = require('./public/index')
 
+router.get('/', (req, res, next) => {
+    res.status(200).json("HALLO GES, WELCOME TO API STAYNESIA BICES")
+})
 router.use('/auth', auth)
 router.use('/private', authMiddleware.authToken, private)
 router.use('/public', public)
