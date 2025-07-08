8487ec2ee5b6a28f31774a3ff423bbd883df5e20
ABAC <yunanwu123@gmail.com>
2025-07-08 21:54:19 +0700

    update upload route

--- server/api/v1/routes/public/index.js
+++ server/api/v1/routes/public/index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const listing = require("./listing");
 const midtrans = require("./midtrans")
-const path = require("path");
+const multer = require('../../middleware/multer')
 
 router.use("/listing", listing);
 router.use("/midtrans", midtrans)
-router.use(
-  "/upload",
-  express.static(path.join(__dirname, "../../../../ListingUploads"))
-);
+router.use("/upload", multer.upload.array('images', 5), (req, res) => {
+  if (!req.files || req.files.length === 0)
+    return res.status(404).json('Failed')
+  return res.status(200).json('Success')
+});
 
 module.exports = router;
