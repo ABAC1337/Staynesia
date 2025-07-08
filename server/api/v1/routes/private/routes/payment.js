086acbaeb7b7b125fc7026d33d520cf95a129781
ABAC <yunanwu123@gmail.com>
2025-07-08 07:51:02 +0700

    update routes ( payment (private), index (public), and midtrans (public)

--- server/api/v1/routes/private/routes/payment.js
+++ server/api/v1/routes/private/routes/payment.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const paymentController = require('../../../controller/paymentController');
 
 router.post('/create', paymentController.createPayment)
-router.post('/notification', paymentController.updateStatus)
 router.route('/:id')
     .delete(paymentController.deletePayment)
 
--- server/api/v1/routes/public/index.js
+++ server/api/v1/routes/public/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const listing = require("./listing");
+const midtrans = require("./midtrans")
 const path = require("path");
 
 router.use("/listing", listing);
+router.use("/midtrans", midtrans)
 router.use(
   "/upload",
   express.static(path.join(__dirname, "../../../../ListingUploads"))
