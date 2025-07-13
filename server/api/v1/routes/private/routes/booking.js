1135c0d61e21a85002e9338c75eceabd6f44f5ce
ABAC <yunanwu123@gmail.com>
2025-07-13 11:16:35 +0700

    update booking route (add cacheHandler middleware)

--- server/api/v1/routes/private/routes/booking.js
+++ server/api/v1/routes/private/routes/booking.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const bookingController = require('../../../controller/bookingController')
+const cacheHandler = require('../../../middleware/cacheHandler')
 const router = express.Router()
 
 router.post('/create', bookingController.createBooking)
 router.post('/update-status', bookingController.updateStatus)
 router.route('/:id')
-    .get(bookingController.getBookingId)
+    .get(cacheHandler.noCache, bookingController.getBookingId)
     .patch(bookingController.updateBooking)
     .delete(bookingController.deleteBooking)
 
-module.exports = router
+module.exports = routerƒƒ
