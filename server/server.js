dc16ba7cdde786e04bbce41c4d4643b361cbc8e9
ABAC <yunanwu123@gmail.com>
2025-07-15 17:41:18 +0700

    update server.js (add scheduler)

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 const router = require('./api/v1/routes/routes')
 const errorHandler = require('./api/v1/middleware/errorHandler')
 const db = require('./config/db')
+const cron = require('./jobs/cron')
 const app = express();
 dotenv.config()
 const PORT = 3000
@@ -16,5 +17,6 @@ app.use(errorHandler)
 
 db.connect().then(() => {
     app.listen(PORT);
+    cron
     console.log(`Server running on http://localhost:${PORT}`);
 });
