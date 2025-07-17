c7bf080e9a81a43e01db08fa80dd3e063cd99406
ABAC <yunanwu123@gmail.com>
2025-07-17 15:13:56 +0700

    update schedule (cron)

--- server/jobs/cron.js
+++ server/jobs/cron.js
@@ -2,9 +2,10 @@ const cron = require('node-cron');
 const update = require('./updateStatusBooking')
 const emailReminder = require('./emailReminder')
 
-cron.schedule('28 19 * * *', async () => {
-    // await update.checkInvalidBookings()
+cron.schedule('00 18 * * *', async () => {
     await update.updateBookingStatusAuto()
-    // await emailReminder.reminderCheckIn()
-    // await emailReminder.reminderCheckOut()
+    await emailReminder.reminderCheckIn()
+    await emailReminder.reminderCheckOut()
+},{
+    timezone: "Asia/Jakarta"
 })
